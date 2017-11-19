f03d8006b43accf4bc56dc4c4cb403d397747645
Vinayak Kankanwadi <vinayakkankanwadi@hotmail.com>
2017-11-19 23:44:07 +1000

    layers

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import csv
 import cv2
 import numpy as np
+import tensorflow as tf
 
 lines = []
-with open('./data/driving_log.csv') as csvfile:
+with open('./data-track1/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -13,7 +14,7 @@ measurements = []
 for line in lines:
 	source_path = line[0]
 	filename = source_path.split('/')[-1]
-	current_path = './data/IMG/'+filename
+	current_path = './data-track1/IMG/'+filename
 	image = cv2.imread(current_path)
 	images.append(image)
 	measurement = float(line[3])
@@ -23,15 +24,52 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 print(len(X_train))
+print(X_train.shape)
 print(len(y_train))
+print(y_train.shape)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Activation, Convolution2D, Cropping2D, Conv2D
 
-model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
-model.add(Dense(1))
+def get_model():
+    model = Sequential()
 
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+    
+    model.add(Conv2D(24,(5, 5), strides=(2, 2)))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(36, (5, 5), strides=(2, 2)))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(48, (5, 5), strides=(2, 2)))
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(64, (3, 3), strides=(1, 1)))
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(64, (3, 3), strides=(1, 1))) 
+    model.add(Activation('relu'))
+
+    model.add(Flatten())    
+
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    
+    model.add(Dense(50))
+    model.add(Activation('relu'))
+    
+    model.add(Dense(10))
+    model.add(Activation('relu'))
+
+    model.add(Dense(1))
+
+    model.compile(optimizer="adam", loss="mse") 
+    
+    return model
+
+model = get_model()
 model.compile(loss='mse',optimizer='adam')
 model.fit(X_train,y_train,validation_split=0.2,shuffle=True,epochs=7)
 model.save('model.h5')
